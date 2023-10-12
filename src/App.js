e0de705a47dba752052cdac883eddf8652c365ed
prasanthibea <jprashanthi@beanalytic.com>
2023-10-12 10:56:55 +0530

    i completed my first task

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function App() {
+  const divStyle = {   
+    border: '2px solid green', 
+    padding: '10px', 
+    margin: '10px', 
+    display: "inline-block" 
+  };
+  return (
+    <div>
+      hai
+      <h1>hello</h1>
+      prasanthi
+      <br></br>
+      python
+      <br></br>
+      <i>hai</i>
+      <br></br>
+      <ins>hello</ins>
+      <br></br>
+      <img src ="https://www.beanalytic.com/wp-content/uploads/2021/07/BeAnalytic-Logo.jpg" alt ="" width = "500px"/>
+
+	  <br></br>
+	
+      <div style={divStyle}>
+        <h2>Student Name</h2>
+        <h3>Town: Nellore</h3>
+        <h3>Phone: 112233</h3>
+		  </div>
+
+    </div>
+    
+  )
+}
